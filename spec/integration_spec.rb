3709bf30b3e4352fd02f86e52f7e1264dbc7b8a5
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-10-30 22:07:08 +0900

    Make sure that the extensions doesn't break the elasticsearch-model

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,5 +1,47 @@
 require 'elasticsearch/model/extensions/all'
 
+RSpec.shared_examples 'a normal elasticsearch-model object' do
+  describe 'a record creation' do
+    before(:each) do
+      ::Article.create(title: 'foo', created_at: Time.now)
+
+      ::Article.__elasticsearch__.refresh_index!
+    end
+
+    it 'makes the document searchable' do
+      expect(Article.search('foo').records.size).to eq(1)
+    end
+  end
+
+  describe 'a record update' do
+    before(:each) do
+      Article.first.update_attributes title: 'Test2'
+
+      Article.__elasticsearch__.refresh_index!
+    end
+
+    it 'makes the document unsearchable using the old content' do
+      expect(Article.search('Test').records.size).to eq(0)
+    end
+
+    it 'makes the document searchable using the new content' do
+      expect(Article.search('Test2').records.size).to eq(1)
+    end
+  end
+
+  describe 'a record deletion' do
+    before(:each) do
+      Article.first.destroy
+
+      Article.__elasticsearch__.refresh_index!
+    end
+
+    it 'makes the document unsearchable' do
+      expect(Article.search('Test').records.size).to eq(0)
+    end
+  end
+end
+
 RSpec.describe 'integration' do
   context 'with articles' do
     before :each do
@@ -12,44 +54,22 @@ RSpec.describe 'integration' do
       end
     end
 
-    describe 'a record creation' do
-      before(:each) do
-        ::Article.create(title: 'foo', created_at: Time.now)
+    it_behaves_like 'a normal elasticsearch-model object'
+  end
 
-        ::Article.__elasticsearch__.refresh_index!
-      end
+  context 'with articles_with_comments_and_delayed_jobs' do
+    before(:each) do
+      load 'setup/articles_with_comments_and_delayed_jobs.rb'
+    end
 
-      it 'makes the document searchable' do
-        expect(Article.search('foo').records.size).to eq(1)
+    after(:each) do
+      ActiveRecord::Schema.define(:version => 2) do
+        drop_table :articles
+        drop_table :comments
+        drop_table :delayed_jobs
       end
     end
 
-    describe 'a record update' do
-      before(:each) do
-        Article.first.update_attributes title: 'Test2'
-
-        Article.__elasticsearch__.refresh_index!
-      end
-
-      it 'makes the document unsearchable using the old content' do
-        expect(Article.search('Test').records.size).to eq(0)
-      end
-
-      it 'makes the document searchable using the new content' do
-        expect(Article.search('Test2').records.size).to eq(1)
-      end
-    end
-
-    describe 'a record deletion' do
-      before(:each) do
-        Article.first.destroy
-
-        Article.__elasticsearch__.refresh_index!
-      end
-
-      it 'makes the document unsearchable' do
-        expect(Article.search('Test').records.size).to eq(0)
-      end
-    end
+    it_behaves_like 'a normal elasticsearch-model object'
   end
 end
