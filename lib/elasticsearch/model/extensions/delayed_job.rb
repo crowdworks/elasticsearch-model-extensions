666b524ee8cec03b22b2c8be1bdfabf3dc609638
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-11-18 15:09:34 +0900

    Add a missing require for delayed_job

--- lib/elasticsearch/model/extensions/delayed_job.rb
+++ lib/elasticsearch/model/extensions/delayed_job.rb
@@ -1,3 +1,4 @@
+require 'delayed_job'
 require_relative 'delayed_job/index_document_job'
 require_relative 'delayed_job/partially_update_document_job'
 require_relative 'delayed_job/delete_document_job'
