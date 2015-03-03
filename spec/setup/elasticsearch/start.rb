3a5ce269dda97534406009283d30bc69ef57eabb
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2015-03-03 20:50:31 +0900

    Disable the multicast ping discovery to successfully start the testing Elasticsearch cluster when there is an another node of Elasticsearch running in your local network

--- spec/setup/elasticsearch/start.rb
+++ spec/setup/elasticsearch/start.rb
@@ -2,4 +2,8 @@ require 'elasticsearch/extensions/test/cluster'
 
 port_to_listen = ENV['TEST_CLUSTER_PORT'] || '9250'
 
-Elasticsearch::Extensions::Test::Cluster.start(nodes:1, port: port_to_listen.to_i)
+Elasticsearch::Extensions::Test::Cluster.start(
+  nodes:1,
+  port: port_to_listen.to_i,
+  es_params: '-D es.discovery.zen.ping.multicast.enabled=false'
+)
