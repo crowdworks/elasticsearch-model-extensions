66f516be26613835fa1f883ea60a0cea10194c98
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2015-03-03 21:01:16 +0900

    When running specs and examples, allow setting up indexes with the kuromoji analyzer via the environment variable TITLE_ANALYZER

--- spec/setup/articles.rb
+++ spec/setup/articles.rb
@@ -13,7 +13,7 @@ class ::Article < ActiveRecord::Base
 
   settings index: {number_of_shards: 1, number_of_replicas: 0} do
     mapping do
-      indexes :title, type: 'string', analyzer: 'snowball'
+      indexes :title, type: 'string', analyzer: (ENV['TITLE_ANALYZER'] || 'snowball')
       indexes :created_at, type: 'date'
     end
   end
--- spec/setup/articles_with_comments_and_delayed_jobs.rb
+++ spec/setup/articles_with_comments_and_delayed_jobs.rb
@@ -50,7 +50,7 @@ class ::Article < ActiveRecord::Base
 
   settings index: {number_of_shards: 1, number_of_replicas: 0} do
     mapping do
-      indexes :title, type: 'string', analyzer: 'snowball'
+      indexes :title, type: 'string', analyzer: (ENV['TITLE_ANALYZER'] || 'snowball')
       indexes :created_at, type: 'date'
       indexes :comments, type: 'object' do
         indexes :body, type: 'string', include_in_all: true
